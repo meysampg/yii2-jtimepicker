624c17c23fb7529053c9042aff9fcef383e62ecb
Meysam <p.g.meysam@gmail.com>
2016-10-04 15:03:27 +0330

    Add time picker widget

---
+++ JTimePicker.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace meysampg\jtimepicker;
+
+use Yii;
+use yii\base\InvalidParamException;
+use yii\helpers\FormatConverter;
+use yii\helpers\Html;
+use yii\helpers\Json;
+use yii\jui\DatePicker as BaseDatePicker;
+use meysampg\jtimepicker\JTimePickerAsset;
+
+class JTimePicker extends BaseDatePicker
+{
+    public function run()
+    {
+        $view = $this->getView();
+
+        echo $this->renderWidget() . "\n";
+
+        $containerID = $this->inline ? $this->containerOptions['id'] : $this->options['id'];
+        $language = $this->language ? $this->language : Yii::$app->language;
+
+        if (strncmp($this->dateFormat, 'php:', 4) === 0) {
+            $this->clientOptions['dateFormat'] = FormatConverter::convertDatePhpToJui(substr($this->dateFormat, 4));
+        } else {
+            $this->clientOptions['dateFormat'] = FormatConverter::convertDateIcuToJui($this->dateFormat, 'date', $language);
+        }
+
+        if ($language !== 'en-US') {
+            $options = Json::htmlEncode($this->clientOptions);
+            $language = Html::encode($language);
+            $view->registerJs("$('#{$containerID}').timepicker($.extend({}, $.datepicker.regional['{$language}'], $options));");
+        } else {
+            $this->registerClientOptions('timepicker', $containerID);
+        }
+
+        $this->registerClientEvents('timepicker', $containerID);
+        JTimePickerAsset::register($view);
+    }
+}
--- README.md
+++ README.md
@@ -30,8 +30,17 @@ Once the extension is installed, simply use it in your code by
 ```php
 <?= \meysampg\jtimepicker\JDateTimePicker::widget(); ?>
 ```
-for a combined Time & Date picker or
+
+![screenshot_2016-10-04_14-58-40](https://cloud.githubusercontent.com/assets/1416085/19072535/94561672-8a42-11e6-95fd-62a3cb0702d8.png)
+
+or
 ```php
 <?= \meysampg\jtimepicker\JDatePicker::widget(); ?>
 ```
-for a single date picker.
+![screenshot_2016-10-04_14-59-02](https://cloud.githubusercontent.com/assets/1416085/19072533/94320f70-8a42-11e6-98ad-51bb82325a8d.png)
+
+or
+```php
+<?= \meysampg\jtimepicker\JTimePicker::widget(); ?>
+```
+![screenshot_2016-10-04_14-58-02](https://cloud.githubusercontent.com/assets/1416085/19072534/9453a98c-8a42-11e6-8953-2074802b1826.png)
